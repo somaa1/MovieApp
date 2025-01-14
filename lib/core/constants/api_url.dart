1bb5be28c3c293c9960c4ee3cd2f6932851b5d1f
ismailrabie <ismailrabie111@gmail.com>
2025-01-14 14:12:54 +0200

    create constants apiUrl

---
+++ lib/core/constants/api_url.dart
@@ -0,0 +1,17 @@
+class ApiUrl {
+
+
+  static const String baseURL = "http://192.168.8.100:5000/api/v1/";
+
+
+  static const signup = '${baseURL}auth/signup';
+  static const signin = '${baseURL}auth/signin';
+  // static const trendingMovies = '${apiV}movie/trending';
+  // static const nowPlayingMovies = '${apiV}movie/nowplaying';
+  // static const popularTV = '${apiV}tv/popular';
+  // static const movie = '${apiV}movie/';
+  // static const tv = '${apiV}tv/';
+  // static const search = '${apiV}search/';
+
+  static const trailerBase = 'https://www.youtube.com/watch?v=';
+}
