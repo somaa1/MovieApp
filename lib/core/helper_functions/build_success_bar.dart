980c5720c27a3cb6d5811bfd40a8e28761b4aafa
ismailrabie <ismailrabie111@gmail.com>
2025-01-14 14:13:44 +0200

    and snackBar to app

---
+++ lib/core/helper_functions/build_success_bar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void buildSuccessBar(BuildContext context, String message) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(message),
+      backgroundColor: Colors.green,
+      behavior: SnackBarBehavior.floating,
+    ),
+  );
+}
