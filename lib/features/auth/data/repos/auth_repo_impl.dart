9c6a075bced1496cb324ef194ba67403cf65d356
ismailrabie <ismailrabie111@gmail.com>
2025-01-17 07:31:48 +0200

    create Repository for Authentication

--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -1,64 +1,57 @@
-import 'dart:ffi';
-
 import 'package:dartz/dartz.dart';
-import 'package:dio/dio.dart';
-import 'package:movieapp/core/Service/auth_api_service/auth_api_service.dart';
 import 'package:movieapp/core/Service/servier_locter.dart';
+
 import 'package:movieapp/features/auth/data/models/user_model.dart';
+import 'package:movieapp/features/auth/domin/repos/auth_repo.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import '../../../../core/Service/shared_prefrence_singelton.dart';
-import '../../../../core/constants/api_url.dart';
-import '../../../../core/network/dio_client.dart';
-import '../../domin/repos/auth_repo.dart';
+import '../../../../core/Service/auth_api_service/auth_api_service.dart';
 
-class AuthRepoImpl implements AuthRepo {
-  Future<Either> signup(UserModel user) async {
-    var result = await getIt<AuthApiService>().signup(user);
-    return result.fold((user) {
-      Prefs.setString('token', user['user']['token']);
-
-      return Right(user);
-    }, (l) {
-      return Left(l);
-    });
-  }
+class AuthRepoImpl extends AuthRepo {
 
 
 
   @override
-  Future<Either<String, UserModel>> signin(UserModel user) async {
-    try {
-      print("Sending request to: ${ApiUrl.signin} with data: ${user.userToMap()}");
-      var response = await getIt<DioClient>().post(
-        ApiUrl.signin,
-        data: user.userToMap(),
-      );
-      print("Response received: ${response.data}");
-
-      if (response.data != null && response.data['user'] != null) {
-        print("UserModel created successfully");
-        return Right(UserModel.fromJson(response.data['user']));
-      } else {
-        print("Invalid response structure");
-        return Left('Invalid response structure');
-      }
-    } on DioException catch (e) {
-      print("Error occurred: ${e.response?.data}");
-      return Left(e.response?.data['message'] ?? 'An unknown error occurred');
-    }
+  Future<Either> signup(UserModel params) async {
+    var data = await sl<AuthService>().signup(params);
+    return data.fold(
+            (error) {
+          return Left(error);
+        },
+            (data) async {
+          final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+          sharedPreferences.setString('token',data['user']['token']);
+          return Right(data);
+        }
+    );
   }
 
+  @override
+  Future<Either> signin(UserModel params) async {
+    var data = await sl<AuthService>().signin(params);
+    return data.fold(
+            (error) {
+          return Left(error);
+        },
+            (data) async {
+          final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+          sharedPreferences.setString('token',data['user']['token']);
+          return Right(data);
+        }
+    );
+  }
 
   @override
   Future<bool> isLoggedIn() async {
-    var token = Prefs.getString('token');
+    final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    var token = sharedPreferences.getString('token');
+    print('Token: $token'); // طباعة الـ token
+
     if (token == null) {
       return false;
     } else {
       return true;
     }
   }
-
-
 
 }
--- lib/features/auth/domin/repos/auth_repo.dart
+++ lib/features/auth/domin/repos/auth_repo.dart
@@ -1,14 +1,13 @@
 import 'dart:ffi';
 
 import 'package:dartz/dartz.dart';
-import 'package:movieapp/core/errors/failure.dart';
 import 'package:movieapp/features/auth/data/models/user_model.dart';
 
 abstract class AuthRepo {
 
   
 Future<Either> signup(UserModel user);
-Future<Either<String,UserModel>> signin(UserModel user);
+Future<Either> signin(UserModel user);
 Future<bool> isLoggedIn();
 
 }
