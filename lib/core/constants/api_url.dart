bb9553c8108d3d5178b40b39f9e6dd8c53fe21b0
ismailrabie <ismailrabie111@gmail.com>
2025-01-23 20:15:40 +0200

    edit api_url.dart

--- lib/core/constants/api_url.dart
+++ lib/core/constants/api_url.dart
@@ -1,16 +1,14 @@
 class ApiUrl {
-
-
   static const String baseURL = "http://192.168.8.100:5000/api/v1/";
-
 
   static const signup = '${baseURL}auth/signup';
   static const signin = '${baseURL}auth/signin';
-   static const trendingMovies = '${baseURL}movie/trending';
-   static const nowPlayingMovies = '${baseURL}movie/nowplaying';
-  // static const popularTV = '${apiV}tv/popular';
-  // static const movie = '${apiV}movie/';
-  // static const tv = '${apiV}tv/';
+  static const trendingMovies = '${baseURL}movie/trending';
+  static const nowPlayingMovies = '${baseURL}movie/nowplaying';
+  static const popularTV = '${baseURL}tv/popular';
+  static const movie = '${baseURL}movie/';
+
+   static const tv = '${baseURL}tv/';
   // static const search = '${apiV}search/';
 
   static const trailerBase = 'https://www.youtube.com/watch?v=';
